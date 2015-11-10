46d4822572e312c1ce122a479a28ba0a8061d396
Matt <matt@119-m.cs.ox.ac.uk>
2015-11-10 16:35:13 +0000

    initial

---
+++ pi-collect/pi-collect.py
@@ -0,0 +1,15 @@
+
+# Get current date, check if we have a database for it
+# If we do, use that as the current DB
+# If not, generate a new db name
+
+# Boot up the ACARS decoder
+
+# Boot up the ACARS server
+
+# Wake up parent at midnight
+#   Add the past days DB name to some log
+#   Start the servers with the new log
+
+
+## Might it be easier interface with the DB and just clear the entries?
---
+++ srv-retrieve.py
@@ -0,0 +1,8 @@
+
+#Grab the log containing the new filenames
+
+#Check that the directory to store in exists
+
+#Copy database over into storage directory
+
+#Merge new DBs with master? Keep originals
