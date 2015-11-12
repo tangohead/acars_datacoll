806224e530c4338e6ce0f04e7cdef6b708d94ca4
Matt <matt@119-m.cs.ox.ac.uk>
2015-11-12 15:58:40 +0000

    changing repo setup

--- pi-collect/config.py
+++
@@ -1,6 +0,0 @@
-db_storage_dir = "/home/matt/Documents/acars/acars_datacoll/pi-collect/acars-data/"
-old_storage_dir = "/home/matt/Documents/acars/acars_datacoll/pi-collect/acars-data-old/"
-logging_dir = "/home/matt/Documents/acars/acars_datacoll/pi-collect/logs"
-acars_dec_dir = "/home/matt/Documents/rtlsdr/acarsdec-3.2"
-acars_srv_dir = "/home/matt/Documents/rtlsdr/acarsdec-3.2"
-backup_period = 6 * 60 * 60
